f0af6590b385791bc2fa63e8f0b6a4c0650bb664
Francesco Sposato <sposato.fs@libero.it>
2026-08-24 19:03:07 +0200

    Pagina Projects e Contatti completate

---
+++ app/projects/this.js
@@ -0,0 +1,10 @@
+import { Colors } from "../../constants/colors";
+import { Layout } from "../../constants/layout";
+import Footer from "../../Components/Footer";
+import Kicker from "../../Components/Kicker";
+import ProjectCard from "../../Components/ProjectCard";
+import { ScrollView, View, StyleSheet } from "react-native";
+
+export default function This() {
+  return <ScrollView></ScrollView>;
+}
